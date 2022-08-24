0321e6e7985ef948f92f15ba9a6a0c223370b2e0
Michael Cen <cenyungamichael@gmail.com>
2022-08-23 20:43:29 -0500

    taller34 Lambda y HMetodos HO

--- src/main/java/com/uce/edu/demo/service/funcional/MainInterfacesFuncionales.java
+++ src/main/java/com/uce/edu/demo/service/funcional/MainInterfacesFuncionales.java
@@ -9,6 +9,9 @@ public class MainInterfacesFuncionales {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ConsumoMetodosHighOrder metodosHG=new ConsumoMetodosHighOrder();
+		
 		//SUPPLER
 //		Clases
 		
@@ -25,6 +28,16 @@ public class MainInterfacesFuncionales {
 		IPersonaSupplier<String> supplierLambdaTE= () ->"Daniel TE2";
 		LOGGER.info("Supplier Lambda: "+supplierLambdaTE.getNombre());
 		
+		//Metodos Hight Order
+		String valorHO=metodosHG.consumirSupplier( () ->"Hola Mundo");
+		LOGGER.info("HO Supplier: "+valorHO);
+		
+		String valorHO1=metodosHG.consumirSupplier( () ->{
+			String valorConsultado="15646768";
+			return valorConsultado;
+		});
+		LOGGER.info("HO Supplier: "+valorHO1);
+		
 		
 //		CONSUMER
 //		Clases
@@ -35,18 +48,50 @@ public class MainInterfacesFuncionales {
 		IPersonaConsumer<String> consumerLambda= cadena -> System.out.println("Prueba Consumer Lambda");
 		consumerLambda.accept("Prueba Consumer");
 		
+		
+		//Metodos Hight Order
+		metodosHG.consumirConsumer(valor -> System.out.println(valor), 2);
+		
 //		PREDICATE
 //		Clases
 //		Lambdas
+		IPersonaPredicate<String> predicateLambda= cadena -> cadena.contains("Z");
+		LOGGER.info("Predicate Lambda: "+predicateLambda.evaluar("Zhong"));
+		
+		//Metodos Hight Order
+		boolean respuesta=metodosHG.consumirPredicate(cadena -> cadena.contains("A"), "Zhong");
+		LOGGER.info("HO Predicate : "+respuesta);
+		
 		
 //		FUNCTION
 //		Clases
 //		Lambdas
+		IPersonaFunction<Integer, String> functionLambda= cadena ->{
+			Integer valor=Integer.parseInt(cadena);
+			Integer valorFinal=valor-2;
+			return valorFinal;
+		};
 		
+		LOGGER.info("Function Lambda: "+functionLambda.aplicar("7"));
+		
+		//Metodos Hight Order
+		String valorFinalF=metodosHG.consumirFunction(valor ->{
+			String retorn=valor.toString()+"A";
+			return retorn;
+		}, 1);
+		
+		LOGGER.info("HO Function : "+valorFinalF);
 		
 //		UNARY OPERATOR(FUNCTION)
 //		Clases
 //		Lambdas
+		
+		IPersonaUnaryOperator<String> unaryLambda= cadena->{
+			String valorFinal=cadena.concat("sufijo");
+			return valorFinal;
+		};
+		
+		LOGGER.info("UnaryOperator Lambda: "+unaryLambda.apply("Juan"));
 	}
 
 }
